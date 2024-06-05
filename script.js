89031d55f6abeedc196dabe4d662657f4b1a7323
Altinsk <119261430+Altinsk@users.noreply.github.com>
2024-06-05 16:16:45 +0100

    opening and closing modal + overlay Ok

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// Variable declaration
+const btnShowModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+// Function open modal
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// Function close modal
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// Showing modal
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', openModal);
+}
+
+// Closing modal
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
